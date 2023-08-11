a8a4760965efa17a04b3bfc9e4c3a06eea75f30e
heron982 <felipedevlp@gmail.com>
2023-08-11 15:50:07 -0300

    Listando consultores

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ Route::get('/', function () {
     return view('layouts.app');
 });
 
-Route::post('/consultores', function (Request $request) {
+Route::post('/consultores', function (Request $request)
+{
     $meses = [
         'Fev' => 'Feb',
         'Mar' => 'Mar',
@@ -38,17 +39,31 @@ Route::post('/consultores', function (Request $request) {
     $date_inicio = date('Y-m-d', strtotime($meses[$request->inicio['mes']] . ' ' . $request->inicio['ano']));
     $date_final = date('Y-m-d', strtotime($meses[$request->final['mes']] . ' ' . $request->final['ano']));
 
-    $sql = "SELECT cli.co_usuario
-	-- CASE WHEN fat.total_imp_inc / 100 > 0 THEN SUM(fat.valor) - (SUM(fat.valor) * fat.total_imp_inc / 100) ELSE SUM(fat.valor) END as renda_liquida,
-	-- sal.brut_salario as custo_fixo,
-    -- SUM(fat.valor) * fat.total_imp_inc / 100 * fat.comissao_cn / 100 as comissao,
-    -- SUM(fat.valor) * fat.total_imp_inc / 100 - sal.brut_salario - SUM(fat.valor) * fat.total_imp_inc / 100 * fat.comissao_cn / 100 as lucro
-FROM cao_fatura fat
-	INNER JOIN cao_os os ON fat.co_os = os.co_os
-	INNER JOIN cao_usuario cli ON cli.co_usuario = os.co_usuario
-	INNER JOIN cao_salario sal ON sal.co_usuario = cli.co_usuario
-WHERE fat.data_emissao BETWEEN DATE('$date_inicio') AND DATE('$date_final')
-GROUP BY cli.co_usuario";
+    $sql = "SELECT
+        cli.co_usuario,
+        JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'data_emissao', fat.data_emissao,
+                'valor_fatura', fat.valor,
+                'imposto', fat.total_imp_inc,
+                'comissao', fat.comissao_cn,
+                'renda_liquida', fat.valor - (fat.total_imp_inc / 100) * fat.valor,
+                'custo_fixo', sal.brut_salario,
+                'comissao', fat.comissao_cn / 100 * (fat.valor - (fat.total_imp_inc / 100 * fat.valor)),
+                'lucro', fat.valor - (fat.total_imp_inc / 100) * fat.valor - sal.brut_salario - ((fat.comissao_cn / 100) * (fat.valor - (fat.total_imp_inc / 100 * fat.valor)))
+            )
+        ) as operacoes
+    FROM
+        cao_fatura fat
+            INNER JOIN
+        cao_os os ON fat.co_os = os.co_os
+            INNER JOIN
+        cao_usuario cli ON cli.co_usuario = os.co_usuario
+            INNER JOIN
+        cao_salario sal ON sal.co_usuario = cli.co_usuario
+    WHERE
+        fat.data_emissao BETWEEN DATE('$date_inicio') AND DATE('$date_final')
+    GROUP BY cli.co_usuario";
 
     $result = DB::select(DB::raw($sql));
 
